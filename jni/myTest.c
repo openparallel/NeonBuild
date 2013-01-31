ddb201b8c4a1f2d5791dddb52004bc66aca743be
BeauJoh <beau@inbeta.org>
2013-01-31 16:31:33 +1100

    added an auto-vectorised version

--- jni/myTest.c
+++ jni/myTest.c
@@ -4,35 +4,79 @@ JNIEXPORT jstring JNICALL
 	Java_com_example_neonbuild_MainActivity_doAllTheThings(JNIEnv* env, jobject thiz){
 		
 		struct timeval start, end;
-		long stpElapsed, noStpElapsed;
+		long stpElapsed, noStpElapsed, autoElapsed;
+		long avgStpElapsed, avgNoStpElapsed, avgAutoElapsed;
+		
 		char rtStr[2048];
-		const int NUM_ELEM = 32768; //32kB is L1 cache (for Samsung Exynos 4412)
+		//const int NUM_ELEM = 32768; //32kB is L1 cache (for Samsung Exynos 4412)
+		const int NUM_ELEM = 2048;
 		const int STP_SIZE = 4;
+		const int AVG_PASS = 10;
 		
 		//do a little bit of simple float arithmetric (vector by scalar)
 		float* src = malloc(sizeof(float)*NUM_ELEM); //eight floats
 		float* dest = malloc(sizeof(float)*NUM_ELEM);
 		
-		//set input array
-		for(int i = 0; i < NUM_ELEM; i++){
-			src[i] = i;
+		//initialise the normal counters
+		stpElapsed=0;
+		noStpElapsed=0;
+		autoElapsed=0;
+		
+		//initialise the average counters
+		avgAutoElapsed=0;
+		avgNoStpElapsed=0;
+		avgStpElapsed=0;
+		
+		for(int j = 0; j<AVG_PASS; j++){
+			//set input array
+			for(int i = 0; i < NUM_ELEM; i++){
+				src[i] = i;
+			}
+			
+			//time it as a whole (no stepping)
+			gettimeofday(&start, NULL);
+			for(int i = 0; i < NUM_ELEM; i ++){
+				dest[i] = src[i]+1;
+			}
+			gettimeofday(&end, NULL);
+			autoElapsed = (end.tv_sec-start.tv_sec)*1000000 + end.tv_usec-start.tv_usec;
+			
+			//reset input array
+			for(int i = 0; i < NUM_ELEM; i++){
+				src[i] = i;
+			}
+			
+			//time it as a whole (no stepping)
+			gettimeofday(&start, NULL);
+			addc_float_c(dest,src,2.0f,NUM_ELEM);
+			gettimeofday(&end, NULL);
+			noStpElapsed = (end.tv_sec-start.tv_sec)*1000000 + end.tv_usec-start.tv_usec;
+			
+			
+			//reset input array
+			for(int i = 0; i < NUM_ELEM; i++){
+				src[i] = i;
+			}
+			
+			//time it stepping
+			gettimeofday(&start, NULL);
+			for(int i = 0; i < NUM_ELEM; i+=STP_SIZE){
+				addc_float_c(dest+i+STP_SIZE, src+i+STP_SIZE, 2.0f, STP_SIZE);
+			}
+			gettimeofday(&end, NULL);
+			stpElapsed = (end.tv_sec-start.tv_sec)*1000000 + end.tv_usec-start.tv_usec;
+			
+			avgAutoElapsed+=autoElapsed;
+			avgNoStpElapsed+=noStpElapsed;
+			avgStpElapsed+=stpElapsed;
 		}
 		
-		//time it as a whole (no stepping)
-		gettimeofday(&start, NULL);
-		addc_float_c(dest,src,2.0f,NUM_ELEM);
-		gettimeofday(&end, NULL);
-		noStpElapsed = (end.tv_sec-start.tv_sec)*1000000 + end.tv_usec-start.tv_usec;
+		//average our runtimes
+		avgAutoElapsed/=AVG_PASS;
+		avgNoStpElapsed/=AVG_PASS;
+		avgStpElapsed/=AVG_PASS;
 		
-		//time it stepping
-		gettimeofday(&start, NULL);
-		for(int i = 0; i < NUM_ELEM; i+=STP_SIZE){
-			addc_float_c(dest+i+STP_SIZE, src+i+STP_SIZE, 2.0f, STP_SIZE);
-		}
-		gettimeofday(&end, NULL);
-		stpElapsed = (end.tv_sec-start.tv_sec)*1000000 + end.tv_usec-start.tv_usec;
-		
-		sprintf(rtStr, "Ne10\t\tnostep:It took %d \n\tstep:It took %d microseconds \n",noStpElapsed,stpElapsed);
+		sprintf(rtStr, "Auto\t\t\tIt took %d \nNe10\tnostep:It took %d \n\t\tstep:It took %d microseconds \n",avgAutoElapsed,avgNoStpElapsed, avgStpElapsed);
 		
 		//free objects
 		//free(src);
